ce73f8b3501437b2a06261ac0fbff4778b3bd3f9
lelishna <fedyaeva95@mail.ru>
2019-04-12 17:57:54 +0500

    Add files via upload

--- parser.py
+++ parser.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 import json 
 
-url = "https://cyberleninka.ru/article/n/gipertrofiya-levogo-zheludochka-serdtsa-diagnostika-posledstviya-i-prognoz"
+#url = "https://cyberleninka.ru/article/n/otsenka-effektivnosti-parallelnyh-algoritmov-dlya-modelirovaniya-mnogosloynogo-perseptrona"
 
 # def get_from_cache(url = ""):
 # 	fname = url.split('/')[-1]
@@ -19,28 +19,46 @@ def get_article_info(url = ""): # rez
 	keywords = []
 	id = 1
 
-	data['url'] = url
-
 	#soup = BeautifulSoup(get_from_cache(url), 'html.parser')
 	soup = BeautifulSoup(requests.get(url).text, 'html.parser')
+	
+	data.update({ 'url' : url })
 
 	name = soup.find('i', itemprop = 'headline').get_text()
-	data['name'] = name
+	data.update({ 'name' : name })
 
 	for a in soup.findAll('li', itemprop = 'author'):
 		authors.update( { id : (a.span.get_text()) } )
 		id += 1
-	data.update({'authors' : authors})
+	if authors:
+		data.update({'authors' : authors})
+	else: 
+		pass
 
-	for k in soup.find('i', itemprop = 'keywords').findAll('span'):
-		keywords.append(k.get_text())
-	data['keywords'] = [ _.lower() for _ in keywords ]
+	try:
+		s = soup.find('i', itemprop = 'keywords').findAll('span')
+		for k in s:
+			keywords.append(k.get_text())
+		data.update({ 'keywords' : [ _.lower() for _ in keywords ]}) 
+	except AttributeError: 
+		pass
+		#data.update({ 'keywords' : [] })
+		# иногда нет ключевых слов 
 
-	anno = soup.find('p', itemprop = 'description').get_text()
-	data['annotation'] = anno
+	try: 
+		anno = soup.find('p', itemprop = 'description').get_text()
+		data.update({ 'annotation' : anno })
+	except AttributeError: 
+		pass
+		#data.update({ 'annotation' : "" })
+		# иногда нет аннотации
 	
-	return data
-	#return json.dumps(data, ensure_ascii=0, indent = 4)
+	#return data
+	return json.dumps(data, ensure_ascii=0, indent = 4)
 
 if __name__ == '__main__':
-	print(get_article_info(url))
+#	print(get_article_info(url))
+	f = open('urls.txt')
+	for line in f.readlines():
+		#print(line)
+		print(get_article_info(line.rstrip()))
